10d2ffcb653b9e5929081a3e7880eb2ae49ab3e9
lauralin08 <lauralin08@gmail.com>
2017-07-30 14:40:22 -0700

    completed composition with props.children video

--- github-battle/app/utils/api.js
+++ github-battle/app/utils/api.js
@@ -1,9 +1,9 @@
 var axios = require('axios');
-
-/* var id = "YOUR_CLIENT_ID";
+  
+var id = "YOUR_CLIENT_ID";
 var sec = "YOUR_SECRET_ID";
-var params = "?client_id=" + id + "&client_secret=" + sec; */
-
+var params = "?client_id=" + id + "&client_secret=" + sec;
+  
 function getProfile (username) {
   return axios.get('https://api.github.com/users/' + username + params)
     .then(function (user) {
